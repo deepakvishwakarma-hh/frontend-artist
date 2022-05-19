e92fbef34e060f3b42ecca19e697022d899d5ea2
Deepak Vishwakarma <79501955+deepakvishwakarma-hh@users.noreply.github.com>
2022-05-19 23:48:17 +0530

    CMS update

--- templates/introduction/index.tsx
+++ templates/introduction/index.tsx
@@ -14,8 +14,7 @@ const Introduction = () => {
             <div className={classes.texts}>
                 <motion.p variants={varients} animate="animate" initial="initial">Hello! <br />
                     Im Deepak Vishwakarma -  curious designer /  developer 👈. Interested in futuristic designs, software user interfaces, science  & development tools, human interaction,
-                    productivity, ethics. <br />
-                    🕵️‍♀️ Currently Finding Opportunities. </motion.p>
+                    productivity, ethics. </motion.p>
             </div>
             <div className={classes.more}>
                 <a href="https://github.com/deepakvishwakarma-hh" target={"_parent"}>  Github</a>
